3f4750798c064576a836e182c16c3f31096dd1fd
Đức Đào <duc.dao.431@gmail.com>
2023-11-25 07:39:35 +0700

    use .env file to config vite.config.js

--- SchoolNexus-Server/vite.config.js
+++ SchoolNexus-Server/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import "dotenv/config";
+
+const port = process.env.PORT || "20700";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	server: {
+		port: port,
+	},
+	plugins: [react()],
+});
