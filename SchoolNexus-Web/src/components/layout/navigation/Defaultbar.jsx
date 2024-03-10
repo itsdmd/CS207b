82057c475b49be94d99fe02f1cbb9c19962918b4
ngynbngoc <95579254+ngynbngoc@users.noreply.github.com>
2024-03-10 16:12:56 +0700

    [ui] navbar

---
+++ SchoolNexus-Web/src/components/layout/navigation/Defaultbar.jsx
@@ -0,0 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import logo from "../../../assets/logo.png"
+
+import { Container, Nav, Navbar, Image } from 'react-bootstrap'
+
+export default function Defaultbar() {
+  return (
+    <div>
+        <Navbar expand="lg" className=" fixted-top border-bottom shadow">
+          <Container >
+            <Navbar.Brand href="#Home" className='d-flex align-center' >
+              <Image src={logo} width={70} height={70} />
+            </Navbar.Brand>
+          </Container>
+        
+          <Nav variant='underline'>
+                <Nav.Link  href="#Home" width='' >
+                  <i class="bi bi-house"> </i> 
+                  Home
+                </Nav.Link> 
+                  
+                <Nav.Link href="#Account" >
+                  <i class="bi bi-person"> </i>
+                   Account
+                </Nav.Link> 
+
+                <Nav.Link href="#" >
+                   <i class="bi bi-box-arrow-right "> </i>
+                    Logout
+                </Nav.Link> 
+              </Nav>    
+        </Navbar>
+    </div>
+
+  )
+}
---
+++ SchoolNexus-Web/src/views/Homepage.jsx
@@ -0,0 +1,10 @@
+import Defaultbar from "../components/layout/navigation/Defaultbar";
+
+export default function Homepage() {
+  return (
+    <div>
+      <Defaultbar/>
+    </div>
+       
+  )
+}
