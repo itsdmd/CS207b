61ef25c355203e9f16e5d55669e309c991123c06
CgSimonn <1959019@itec.hcmus.edu.vn>
2024-03-12 14:05:39 +0700

    redirect-page-fix

--- SchoolNexus-Web/src/App.jsx
+++ SchoolNexus-Web/src/App.jsx
@@ -1,35 +1,13 @@
 import LoginPage from "./views/Login";
 import Timetable from "./components/layout/body/Timetable";
 import HomePage from "./views/Home";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-} from "react-router-dom";
+import GlobalRouter from "./services/router/router";
 
 
 const App = () => {
     return (
         <div> 
-            <Router>
-                <Routes>
-                    <Route
-                        exact
-                        path="/"
-                        element= {<HomePage />}
-                    />
-                    <Route
-                        path = "/viewtables"
-                        element = {<Timetable />}
-                    />
-
-                    <Route
-                        path="*"
-                        element={<Navigate to="/" />}
-                    />
-                </Routes>
-            </Router>
+            <GlobalRouter />
         </div>
     );
 };
---
+++ SchoolNexus-Web/src/services/router/router.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Timetable from '../../components/layout/body/Timetable';
+import HomePage from '../../views/Home';
+import LoginPage from '../../views/Login';
+
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+
+export default function GlobalRouter() {
+  return (
+    <Router>
+            <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element= {<HomePage />}
+                    />
+                    <Route
+                        path = "/viewtables"
+                        element = {<Timetable />}
+                    />
+                    <Route
+                        path = "/viewgrades"
+                        element = {<Timetable />}
+                    />
+
+                    <Route
+                        path="*"
+                        element={<Navigate to="/" />}
+                    />
+            </Routes>
+    </Router>
+  )
+}
