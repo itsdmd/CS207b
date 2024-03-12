d4594e832b51af64c4955eed5a75a33d9813f4fe
CgSimonn <1959019@itec.hcmus.edu.vn>
2024-03-12 13:54:36 +0700

    redirect-pages-from-home-minh

--- SchoolNexus-Web/src/App.jsx
+++ SchoolNexus-Web/src/App.jsx
@@ -1,11 +1,35 @@
 import LoginPage from "./views/Login";
 import Timetable from "./components/layout/body/Timetable";
+import HomePage from "./views/Home";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+
 
 const App = () => {
     return (
-        <div>
-            {/* <LoginPage />  */}
-            <Timetable />
+        <div> 
+            <Router>
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element= {<HomePage />}
+                    />
+                    <Route
+                        path = "/viewtables"
+                        element = {<Timetable />}
+                    />
+
+                    <Route
+                        path="*"
+                        element={<Navigate to="/" />}
+                    />
+                </Routes>
+            </Router>
         </div>
     );
 };
--- SchoolNexus-Web/src/components/layout/body/CardsContainer.jsx
+++ SchoolNexus-Web/src/components/layout/body/CardsContainer.jsx
@@ -1,38 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Col, Row } from 'react-bootstrap'
 import PageCard from './HomepageCard';
-
 import img from "../../../assets/cardphoto.png";
+import Timetable from './Timetable';
+import { Link } from 'react-router-dom';
 
 const Cards = () => {
     return (
         <div>
             <Row>
+            <Link to="/viewtables">
                 <Col xs={6} md={4} lg={3} >
-                <PageCard img={img} title = "View Grades" />
+                    <PageCard img={img} title = "View Tables"/>
                 </Col>
-
-                <Col xs={6} md={4} lg={3} >
+            </Link>
+            
+            <Col xs={6} md={4} lg={3} >
                 <PageCard img={img} title = "View Grades"/>
-                </Col>
+            </Col>
 
-                <Col xs={6} md={4} lg={3} >
+            <Col xs={6} md={4} lg={3} >
                 <PageCard img={img} title = "View Grades"/>
-                </Col>
+            </Col>
 
-                <Col xs={6} md={4} lg={3} >
+            <Col xs={6} md={4} lg={3} >
                 <PageCard img={img} title = "View Grades"/>
-                </Col>
+            </Col>
 
-                <Col xs={6} md={4} lg={3} >
+            <Col xs={6} md={4} lg={3} >
                 <PageCard img={img} title = "View Grades"/>
-                </Col>
-
-                <Col xs={6} md={4} lg={3} >
-                <PageCard img={img} title = "View Grades"/>
-                </Col>
-
-
+            </Col>             
 
           </Row>
         </div>
