bbaf11f735eba4cf0777ee5c746efb67beab7cd5
ngynbngoc <95579254+ngynbngoc@users.noreply.github.com>
2024-03-15 21:43:12 +0700

    [ui-Principle] add background

--- SchoolNexus-Web/src/views/Principle/TimetableEdit.jsx
+++ SchoolNexus-Web/src/views/Principle/TimetableEdit.jsx
@@ -2,14 +2,24 @@ import React from "react";
 
 import Defaultbar from "../../components/layout/navigation/Defaultbar";
 import TimetableForm from "../../components/layout/body/Principle/TimetableForm";
+import { Container, Navbar } from "react-bootstrap";
 
 export default function TimetableEdit() {
     return (
-        <div>
+        <div
+            style={{
+                background:
+                    "url(https://t4.ftcdn.net/jpg/01/68/20/47/360_F_168204719_aWhXPHpnQGmwyZ9BSaZAdfMcvNWqle7B.jpg)",
+                backgroundSize: "cover",
+                height: "100vh",
+            }}>
             <Defaultbar />
-            <div className="m-5 w-50">
-                <TimetableForm />
-            </div>
+            <Container className="d-flex text-center align-center flex-column mt-5">
+                <h2 style={{ color: "white" }}>Edit Timetable</h2>
+                <Container className="d-flex justify-content-center  mt-3 ">
+                    <TimetableForm />
+                </Container>
+            </Container>
         </div>
     );
 }
